a0a1427114358f4c7dbd32cc239f89423d54a589
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-17 15:24:45 +0600

    finished withdraw site

---
+++ JS/withdraw.js
@@ -0,0 +1,24 @@
+
+
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+
+    withdrawField.value = '';
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal =parseFloat(previousWithdrawTotalString);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText =newWithdrawTotal;
+
+    const balanceElement =document.getElementById('balance-total');
+    const prevoiusBalanceTotalString = balanceElement.innerText;
+    const previousBalanceTotal = parseFloat(prevoiusBalanceTotalString);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceElement.innerText = newBalanceTotal;
+})
