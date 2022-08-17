5e0a61948d34c42b73242d67adfdc6d205deccd8
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-17 14:05:13 +0600

    deposite part finished

--- JS/deposit.js
+++ JS/deposit.js
@@ -12,14 +12,21 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     const depositField = document.getElementById('deposit-field');
 
     const newDepositAmountString = depositField.value;
-   const newDepositAmount = parseFloat(newDepositAmountString);
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
     depositField.value = '';
 
-   const depositTotalElement = document.getElementById('deposit-total');
+    const depositTotalElement = document.getElementById('deposit-total');
 
-const previousDepositTotalString = depositTotalElement.innerText;
-const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
 
-const newDepositTotal = previousDepositTotal + newDepositAmount;
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const totalBalanceAmountString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(totalBalanceAmountString);
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
 })
